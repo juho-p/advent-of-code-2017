8fdab156109a7bf77ee801f28bed818b23dd0114
Juho Peltonen <juhopel@gmail.com>
2017-12-02 21:11:09 +0200

    d2

---
+++ d2.hs
@@ -0,0 +1,23 @@
+readTable :: String -> [[Int]]
+readTable s = map (map read . words) $ lines s
+
+rowDifference xs = high - low
+    where low = minimum xs
+          high = maximum xs
+
+part1 = sum . map rowDifference
+
+rowDivision xs = head [ d |
+        a <- xs,
+        b <- xs,
+        a > b,
+        let (d,r) = a `divMod` b,
+        r == 0 ]
+
+part2 = sum . map rowDivision
+
+main = do
+    input <- readFile "d2.txt"
+    let table = readTable input
+    print $ part1 table
+    print $ part2 table

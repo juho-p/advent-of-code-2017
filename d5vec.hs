ac8dd650a3ff839a093a2773c481974dc579e1eb
Juho Peltonen <juhopel@gmail.com>
2017-12-06 16:58:02 +0200

    lame experiment

---
+++ d5vec.hs
@@ -0,0 +1,36 @@
+{-# LANGUAGE BangPatterns #-}
+
+-- super slow (proving that updating single item in Data.Vector is just sad)
+
+import qualified Data.Vector.Unboxed as V
+
+parse :: String -> [Int]
+parse = map read . words
+
+type Executor = V.Vector Int -> Int -> Maybe (Int, V.Vector Int)
+
+exec :: (Int -> Int) -> Executor
+exec f v i = case v V.!? i of
+    Nothing -> Nothing
+    Just x -> Just (i + x, v V.// [(i, f x)])
+
+simpleExec :: Executor
+simpleExec = exec (+1)
+
+advancedExec :: Executor
+advancedExec = exec (\x -> if x >= 3 then x - 1 else x + 1)
+
+steps :: Executor -> [Int] -> Int
+steps f offsets = steps' f 0 0 $ V.fromList offsets
+
+steps' :: Executor -> Int -> Int -> V.Vector Int -> Int
+steps' f !cnt !index v = case f v index of
+    Nothing -> cnt
+    Just (index', v') -> steps' f (cnt + 1) index' v'
+
+main :: IO ()
+main = do
+    input <- readFile "d5.txt"
+    let offsets = parse input
+    print $ steps simpleExec offsets
+    --print $ steps advancedExec offsets

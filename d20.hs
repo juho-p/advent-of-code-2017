26e08c786a20f60f5a082fd3a59aec0babfae911
Juho Peltonen <juhopel@gmail.com>
2018-01-14 14:09:45 +0200

    d20

--- d20.hs
+++ d20.hs
@@ -2,6 +2,7 @@ import Text.Parsec
 import Text.Parsec.String
 
 import Data.List
+import qualified Data.Set as Set
 
 type Vec3 = (Int, Int, Int)
 
@@ -32,13 +33,30 @@ particle = do
 particles :: Parser [Particle]
 particles = many particle
 
-len (x,y,z) = abs x + abs y + abs z
-
 part1 xs = head xs'
-    where score (Particle p v a) = (len a, len v, len p)
-          xs' = sortOn (score . snd) (zip [0..] xs)
+    where
+        score (Particle p v a) = (len a, len v, len p)
+        xs' = sortOn (score . snd) (zip [0..] xs)
+        len (x,y,z) = abs x + abs y + abs z
+
+part2 xs = map length $ iterate updateAll xs
+    where
+        update (Particle p v a) =
+            let
+                v' = v `add3` a
+                p' = p `add3` v'
+            in Particle p' v' a
+        add3 (a,b,c) (d,e,f) = (a+d, b+e, c+f)
+        updateAll xs' =
+            let ys = update <$> xs'
+                ks = kills ys
+            in filter (not . (`Set.member` ks) . position) ys
+        kills xs' = snd $ foldl' (\(ps, ks) (Particle p _ _) ->
+            if Set.member p ps then (ps, Set.insert p ks)
+            else (Set.insert p ps, ks)) (Set.empty, Set.empty) xs'
 
 main :: IO ()
 main = do
     xs <- parseFromFile particles "d20.txt"
     print (part1 <$> xs)
+    print (part2 <$> xs)
